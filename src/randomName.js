1919dbaf01957de03036d1a13ab2ef70007cd237
Adam Oleszko <adamoleszko44@gmail.com>
2018-08-19 18:09:43 +0200

    update news box

--- src/randomName.js
+++ src/randomName.js
@@ -1,16 +1,17 @@
 import 'babel-polyfill';
 import DOMLoader from './DOMLoader.js';
+import axios from 'axios';
 
 export default class RandomName {
       // createName(item, randomUser) {
       //   item.innerHTML = randomUser.results[0].name.first[0].toUpperCase() + randomUser.results[0].name.first.substr(1) + ' ' 
       //   + randomUser.results[0].name.last[0].toUpperCase() + randomUsser.results[0].name.last.substr(1);
       // }
-
       changeText(item) {
+        var thiss = this;
         axios.get('https://randomuser.me/api')
         .then(function (response) {
-            // this.createName(item, response.data);
+          // thiss.createName(item, response.data)
             item.innerHTML = response.data.results[0].name.first[0].toUpperCase() + response.data.results[0].name.first.substr(1) + ' ' 
             + response.data.results[0].name.last[0].toUpperCase() + response.data.results[0].name.last.substr(1);
         })
