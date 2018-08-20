8ae85c40707b220d79e265558b1dd040c7304e84
Mateusz Przeliorz <mateuszprzeliorz@outlook.com>
2018-08-20 13:59:07 +0200

    babel and webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./scripts/pop-up-chat.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/dist'
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js"]
+  }
+};
