8647976e30ffb5ed6d8488bc913f3b03f51e96cd
Kryspinn <33183934+Kryspinn@users.noreply.github.com>
2018-08-20 17:57:20 +0200

    login page

---
+++ src/loginScript.js
@@ -0,0 +1,31 @@
+import 'babel-polyfill';
+import axios from 'axios';
+
+const login = document.querySelector("#login");
+const password = document.querySelector("#password");
+document.querySelector("#btn").addEventListener("click", loginn);
+
+
+function loginn() {
+  axios
+    .get("https://api.myjson.com/bins/6iwyw")
+    .then(function(response) {
+      validation(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    }); 
+}
+
+function validation(data){
+  data.forEach(element => {
+    console.log(element.login);
+    if(login.value==element.login && password.value == element.password){
+      redirectToDashboard();
+    }
+  });
+}
+
+function redirectToDashboard(){
+  window.location.replace("/index.html");
+}
